9dc59cf8a00e8c8298da197ee04be86ce1d09e33
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-12 17:06:06 +0900

    fix : Fix returning difference between old and new stock lists interchanged

--- scripts/main.py
+++ scripts/main.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     SQL_password = config('SQL_password', default='')
     db_name = 'us_stock'
 
-    old_stock_list = stocklist.get_stock_list()
+    latest_stock_list = stocklist.get_stock_list()
     db = database.Database(SQL_password, db_name)
-    stock_list = db.read_db()
-    stocklist.check_diff(old_stock_list, stock_list)
-
-    # print(stock_list.head())
+    old_stock_list = db.read_db()
+    diff = stocklist.check_diff(old_stock_list, latest_stock_list)
+    print(diff)
+    
