a8c97c5b157420d82ecc56581fe680b03b30d3f2
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-12 18:04:53 +0900

    feat : Add adding and deleting changes found in latest stock list

--- scripts/database.py
+++ scripts/database.py
@@ -36,7 +36,37 @@ class Database:
             return stock_list
         except Exception as e:
             print(e)
-    
+
+    def delete_stock(self, table_name, diff):
+        # stocks only in past stock list
+        old = diff[diff['_merge']=='left_only']
+        
+        # delete stock from stock list
+        try:
+            for i in old['Symbol']:
+               query = 'DELETE FROM '+table_name+' WHERE Symbol=%s'
+               self.cursor.execute(query, i)
+
+            self.connection.commit()
+
+        except Exception as e:
+            print(e)
+
+    def add_stock(self, table_name, diff):
+        # stocks only in latest stock list
+        new = diff[diff['_merge']=='right_only']
+
+        # add new stock list
+        try:
+            for i in new.to_numpy():
+                query = 'INSERT INTO '+table_name+' VALUES(%s, %s, %s)'
+                self.cursor.execute(query, (i[0], i[1], i[2]))
+            
+            self.connection.commit()
+            
+        except Exception as e:
+            print(e)
+
     def __del__(self):
         """close database connection
         """
