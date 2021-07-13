c63e22b1ca4d41f1fec63a52d0a59261f99e08ea
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-13 18:09:32 +0900

    feat : Add conditionals for checking if stock list is stored in the database

--- scripts/main.py
+++ scripts/main.py
@@ -1,15 +1,22 @@
 import stocklist
 import database
 from decouple import config
-import pandas as pd
 
 if __name__ == '__main__':
     SQL_password = config('SQL_password', default='')
-    db_name = 'us_stock'
-
-    latest_stock_list = stocklist.get_stock_list()
+    db_name = 'us_stock' # same db and table name
     db = database.Database(SQL_password, db_name)
-    old_stock_list = db.read_db()
-    diff = stocklist.check_diff(old_stock_list, latest_stock_list)
-    print(diff)
+    
+    latest_stock_list = stocklist.get_stock_list()
+    
+    # check if stock list is in the database
+    ret = db.check_table_exists()
+    if ret == 1:
+        old_stock_list = db.read_db()
+        diff = stocklist.check_diff(old_stock_list, latest_stock_list)
+        print(diff)
+    elif ret == 0:
+        print('Stock list doensn\'t exists. Cannot compare the difference between past and latest stock list. \n Saving in progress.')
+        db.save_in_db(latest_stock_list, db_name)
+
     
