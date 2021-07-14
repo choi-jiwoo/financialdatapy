2add361c6efc3c41d71c83df4146c9742df5487a
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-15 00:48:36 +0900

    feat : Add creating database if there's no existing us_stock database to store data

--- scripts/main.py
+++ scripts/main.py
@@ -4,26 +4,31 @@ from decouple import config
 
 if __name__ == '__main__':
     SQL_password = config('SQL_password', default='')
-    db_name = 'us_stock' # same db and table name
+    db_name = 'us_stock'
+    table_name = 'stock_list'
+    # connect database server
     db = database.Database(SQL_password, db_name)
+    # create database if it's first time
+    db.create_database()
     
-    # get stock list data from source
+    # get latest stock list data from source
     latest_stock_list = stocklist.get_stock_list()
     
-    # check if stock list is in the database
+    # check if table for stock list exists in the database
     ret = db.check_table_exists()
     if ret == 1:
         old_stock_list = db.read_db()
         diff = stocklist.check_diff(old_stock_list, latest_stock_list)
 
+        # check if there is a difference
         if diff.empty:
             print("No difference")
         else :
             # update stock list to the latest
-            db.delete_stock(db_name, diff)
-            db.add_stock(db_name, diff)
+            db.delete_stock(table_name, diff)
+            db.add_stock(table_name, diff)
     elif ret == 0:
         print('Stock list doensn\'t exists. Cannot compare the difference between past and latest stock list. \n Saving in progress.')
-        db.save_in_db(latest_stock_list, db_name)
+        db.save_in_db(latest_stock_list, table_name)
 
     
