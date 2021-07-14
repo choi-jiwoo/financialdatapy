f9393b435bc5bb989dd3033bfffc46261b161a49
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-14 23:29:24 +0900

    feat : Add deleting and adding stock in stock list in database

--- scripts/main.py
+++ scripts/main.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     db_name = 'us_stock' # same db and table name
     db = database.Database(SQL_password, db_name)
     
+    # get stock list data from source
     latest_stock_list = stocklist.get_stock_list()
     
     # check if stock list is in the database
@@ -14,7 +15,10 @@ if __name__ == '__main__':
     if ret == 1:
         old_stock_list = db.read_db()
         diff = stocklist.check_diff(old_stock_list, latest_stock_list)
-        print(diff)
+
+        # update stock list to the latest
+        db.delete_stock(db_name, diff)
+        db.add_stock(db_name, diff)
     elif ret == 0:
         print('Stock list doensn\'t exists. Cannot compare the difference between past and latest stock list. \n Saving in progress.')
         db.save_in_db(latest_stock_list, db_name)
