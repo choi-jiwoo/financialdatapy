b4ba0aa4234d01a958d621d4e6a9a1f680df4f68
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-20 17:13:18 +0900

    Create cik.py
    Get cik number from sec.gov

---
+++ scripts/cik.py
@@ -0,0 +1,12 @@
+import requests
+import json
+import pandas as pd
+
+def get_cik(url):
+    res = requests.get(url)
+    data = json.loads(res.text)
+
+    cik_list = pd.DataFrame(data['data'], columns=data['fields'])
+    cik_list['name'] = cik_list['name'].str.lower().str.title()
+
+    return cik_list
