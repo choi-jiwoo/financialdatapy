7d22c969b0bee1df9030ac9d3b63cd35080932e8
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-23 00:17:43 +0900

    refactor : Change getting data from source directly to calling 'request_data' from financials.py to get data

--- scripts/filings.py
+++ scripts/filings.py
@@ -1,17 +1,16 @@
-import requests
-import json
+import financials
 import pandas as pd
 
 # test for Netflix
-cik = '0001065280'
-url = f'http://data.sec.gov/submissions/CIK{cik}.json'
-headers = {'User-Agent' : 'Mozilla'}
+def get_filings_list(cik): 
+    cik = '0001065280'
+    url = f'http://data.sec.gov/submissions/CIK{cik}.json'
+    data = financials.request_data(url)
 
-s = requests.get(url, headers)
-submission = json.loads(s.text)
+    acc = data['filings']['recent']['accessionNumber']
+    acc = [s.replace('-', '') for s in acc]
+    form = data['filings']['recent']['form']
+    filings = pd.DataFrame(zip(form, acc), columns=['AccessionNumber', 'Form'])
+    latest_filing = filings[filings['AccessionNumber']=='10-K'].iloc[0].at['Form']
 
-acc = submission['filings']['recent']['accessionNumber']
-acc = [s.replace('-', '') for s in acc]
-form = submission['filings']['recent']['form']
-filings = pd.DataFrame(zip(form, acc), columns=['AccessionNumber', 'Form'])
-latest_filing = filings[filings['AccessionNumber']=='10-K'].iloc[0].at['Form']
+    return latest_filing
