525db3055f07e892d40ec6fd38e81ea068e293e8
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-26 23:07:03 +0900

    Create main.py

---
+++ scripts/main.py
@@ -0,0 +1,29 @@
+import financials
+import filings
+
+if __name__ == '__main__':
+    # Getting cik list
+    cik_list = filings.get_cik()
+
+    # Getting data from SEC
+    ticker = input('ticker : ') # should add test code later
+    cik_num = filings.search_cik(cik_list, ticker)
+
+    # Getting list of submitted filings 
+    submission = filings.get_filings_list(cik_num)
+    latest_10K_filing = submission[submission['AccessionNumber']=='10-K'].iloc[0].at['Form']
+
+    # get latest 10-K 
+    links = filings.get_latest_10K(cik_num, latest_10K_filing)
+
+    # get facts
+    is_l = links.get('income_statement')
+    income_statement = financials.get_facts(is_l)
+
+    bs_l = links.get('balance_sheet')
+    balance_sheet = financials.get_facts(bs_l)
+
+    cf_l = links.get('cash_flow')
+    cash_flow = financials.get_facts(cf_l)
+
+
