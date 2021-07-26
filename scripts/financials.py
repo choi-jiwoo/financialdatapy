f3a94ca5b4eb8f74250a1c7573519f2979add5fc
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-26 20:41:00 +0900

    feat : Add getting links of 3 major financial statements data

--- scripts/financials.py
+++ scripts/financials.py
@@ -2,25 +2,32 @@ import re
 import filings
 from bs4 import BeautifulSoup
 
+def get_facts(cik, filing, link):
+    
+
 def get_latest_10K(cik, latest):
     url = f'https://www.sec.gov/cgi-bin/viewer?action=view&cik={cik}&accession_number={latest}&xbrl_type=v'
     res = filings.request_data(url)
     soup = BeautifulSoup(res.text)
 
-    financial_stmt = soup.find(id='menu_cat3')
-    ul = financial_stmt.find_next('ul')
+    menu = soup.find(id='menu')
+    a = menu.find_next('a', string='Financial Statements')
+    ul = a.find_next('ul')
     li = ul.find_all('li')
 
-    financials_name = [x.get_text() for x in li]
-    file_names = [re.search('r\d', str(x)).group().upper() for x in li]
+    # element names in financial statements
+    element = [x.get_text() for x in li]
+    # filenames of each 3 statements
+    filename = [re.search('r\d', str(x)).group().upper() for x in li]
+    file_list = list(zip(element, filename))
 
-    get_facts(cik, latest, file_names)
+    # remain links only income statements, cash flows, and balance sheets
+    filename_dict = {}
+    pattern = '((((?<!comprehensive)\sincome)|operation+s?|earning+s?)|(cash\sflow+s?)|(balance\ssheet+s?))(?!\(parenthetical+s?\))([a-z0-9]+)$'
+    regex = re.compile(pattern, flags=re.IGNORECASE)
 
-def get_facts(cik, filing, file_names):
-    # test just for one statment for now
-    file = file_names[0]
-    url = f'https://www.sec.gov/Archives/edgar/data/{cik}/{filing}/{file}.htm'
+    for i in file_list:
+        if regex.search(i[0]):
+            filename_dict[i[0]] = f'https://www.sec.gov/Archives/edgar/data/{cik}/{latest}/{i[1]}.htm'
     
-    # scrape codes ...
-
-    
+    get_facts(cik, latest, filename_dict)
