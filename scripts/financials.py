cffe65b99b1033b889d2880ece995f41a5757042
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-26 22:19:21 +0900

    refactor : Remove functions that moved to filings.py

--- scripts/financials.py
+++ scripts/financials.py
@@ -1,33 +1,38 @@
-import re
 import filings
 from bs4 import BeautifulSoup
+import pandas as pd
 
-def get_facts(cik, filing, link):
-    
-
-def get_latest_10K(cik, latest):
-    url = f'https://www.sec.gov/cgi-bin/viewer?action=view&cik={cik}&accession_number={latest}&xbrl_type=v'
-    res = filings.request_data(url)
+def get_facts(link):
+    res = filings.request_data(link)
     soup = BeautifulSoup(res.text)
 
-    menu = soup.find(id='menu')
-    a = menu.find_next('a', string='Financial Statements')
-    ul = a.find_next('ul')
-    li = ul.find_all('li')
+    tbl = soup.find('table')
+    # which financial
+    title = tbl.find(class_='tl').get_text()
+    # index 0 : which financial
+    # index 1: unit (USD)
+    title.split(' - ')
 
-    # element names in financial statements
-    element = [x.get_text() for x in li]
-    # filenames of each 3 statements
-    filename = [re.search('r\d', str(x)).group().upper() for x in li]
-    file_list = list(zip(element, filename))
+    # date
+    date = tbl.find_all(class_='th')
+    date = [x.get_text() for x in date]
+    date.pop(0) # remove unnecessary
 
-    # remain links only income statements, cash flows, and balance sheets
-    filename_dict = {}
-    pattern = '((((?<!comprehensive)\sincome)|operation+s?|earning+s?)|(cash\sflow+s?)|(balance\ssheet+s?))(?!\(parenthetical+s?\))([a-z0-9]+)$'
-    regex = re.compile(pattern, flags=re.IGNORECASE)
+    # element names
+    element_tbl = tbl.find_all(class_='pl')
+    element = [x.get_text() for x in element_tbl]
+    # facts
+    facts_tbl = tbl.find_all(class_=['nump', 'num', 'text'])
+    facts = [x.get_text().strip() for x in facts_tbl]
 
-    for i in file_list:
-        if regex.search(i[0]):
-            filename_dict[i[0]] = f'https://www.sec.gov/Archives/edgar/data/{cik}/{latest}/{i[1]}.htm'
-    
-    get_facts(cik, latest, filename_dict)
+    num_of_facts_y = len(facts) // len(date)
+    years = len(date)%len(facts)
+
+    # key = date : value = facts
+    facts_consecutive = dict({date[x] : facts[x::years] for x in range(0,years)})
+
+    # check if number of elements in a financial statement match with number of facts
+    if len(element) == num_of_facts_y:
+        facts_df = pd.DataFrame(facts_consecutive, index=element)
+
+    return facts_df
