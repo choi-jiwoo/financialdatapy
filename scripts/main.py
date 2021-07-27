55aead72389a02e8eb3ab3d7f96ad366d1e9684d
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-27 14:35:11 +0900

    feat : Add case when no 10-K was submitted

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,4 @@
+from numpy import ushort
 import financials
 import filings
 
@@ -11,19 +12,21 @@ if __name__ == '__main__':
 
     # Getting list of submitted filings 
     submission = filings.get_filings_list(cik_num)
-    latest_10K_filing = submission[submission['Form']=='10-K'].iloc[0].at['AccessionNumber']
 
-    # get latest 10-K 
-    links = filings.get_latest_10K(cik_num, latest_10K_filing)
+    if submission[submission['Form']=='10-K'].empty: print('No 10-K submitted.')
+    else:
+        # get latest 10-K 
+        latest_10K_filing = submission[submission['Form']=='10-K'].iloc[0].at['AccessionNumber']
+        links = filings.get_latest_10K(cik_num, latest_10K_filing)
 
-    # get facts
-    is_l = links.get('income_statement')
-    income_statement = financials.get_facts(is_l)
+        # get facts
+        is_l = links.get('income_statement')
+        income_statement = financials.get_facts(is_l)
 
-    bs_l = links.get('balance_sheet')
-    balance_sheet = financials.get_facts(bs_l)
+        bs_l = links.get('balance_sheet')
+        balance_sheet = financials.get_facts(bs_l)
 
-    cf_l = links.get('cash_flow')
-    cash_flow = financials.get_facts(cf_l)
+        cf_l = links.get('cash_flow')
+        cash_flow = financials.get_facts(cf_l)
 
 
