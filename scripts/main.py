e897f34600955ac4f4ac7e9599c6a2a58984b0a8
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-28 18:30:35 +0900

    refactor : Remove getting company 10-k facts

--- scripts/main.py
+++ scripts/main.py
@@ -1,33 +1,28 @@
-from numpy import ushort
 import financials
 import filings
+import cik
+import connect
 
 def main():
     # Getting cik list
-    cik_list = filings.get_cik()
+    cik_list = cik.get_cik()
+
+    db = connect.connect_db()
+    db.save_in_db(cik_list)
 
     # Getting data from SEC
     ticker = input('ticker : ') # should add test code later
-    cik_num = filings.search_cik(cik_list, ticker)
+    cik_num = cik.search_cik(cik_list, ticker)
 
     # Getting list of submitted filings 
     submission = filings.get_filings_list(cik_num)
 
-    if submission[submission['Form']=='10-K'].empty: print('No 10-K submitted.')
-    else:
-        # get latest 10-K 
-        latest_10K_filing = submission[submission['Form']=='10-K'].iloc[0].at['AccessionNumber']
-        links = filings.get_latest_10K(cik_num, latest_10K_filing)
-
-        # get facts
-        is_l = links.get('income_statement')
-        income_statement = financials.get_facts(is_l)
-
-        bs_l = links.get('balance_sheet')
-        balance_sheet = financials.get_facts(bs_l)
-
-        cf_l = links.get('cash_flow')
-        cash_flow = financials.get_facts(cf_l)
+    income_statement, balance_sheet, cash_flow = financials.get_10K_facts(
+        cik_num,
+        submission
+    )
+    
+    # Do something with these financial statements data...
 
 if __name__ == '__main__':
     main()
