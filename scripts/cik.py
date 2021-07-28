0b907d27db6ab94f96afd5b00ce4f255a386ccd5
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-28 17:39:53 +0900

    refactor : Modularize getting cik list from source

---
+++ scripts/cik.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import request
+
+def get_cik():
+    url = 'https://www.sec.gov/files/company_tickers_exchange.json'
+    data = request.request_data(url, 'json')
+    
+    cik_list = pd.DataFrame(data['data'], columns=data['fields'])
+    cik_list['name'] = cik_list['name'].str.lower().str.title()
+
+    return cik_list
+
+def search_cik(cik_list, ticker):
+    ticker = ticker.upper()
+    data = cik_list[cik_list['ticker']==ticker]['cik']
+    data = data.values[0]
+
+    # cik number received from source excludes 0s that comes first.
+    # Since cik is a 10-digit number, concatenate 0s.
+    zeros = 10 - len(str(data))
+    data = ('0' * zeros) + str(data)
+    return data
