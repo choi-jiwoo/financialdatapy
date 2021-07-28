11f4e224b9299ff8c0c33e93169b401e583ccc60
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-28 17:12:13 +0900

    refactor : Change method of removing non-date variable from date section in financial statement

--- scripts/financials.py
+++ scripts/financials.py
@@ -1,37 +1,39 @@
 import filings
-from bs4 import BeautifulSoup
-import pandas as pd
 import re
+import pandas as pd
+from bs4 import BeautifulSoup
+from dateutil.parser import parse
 
 def get_facts(link):
     res = filings.request_data(link)
     soup = BeautifulSoup(res.text, 'html.parser')
     tbl = soup.find('table')
 
-    date = tbl.find_all(class_ = 'th')
-    date = [x.get_text() for x in date]
+    facts_hdr = tbl.find_all(class_ = 'th')
+    facts_hdr = [x.get_text() for x in facts_hdr]
 
     title = tbl.find(class_ = 'tl').get_text()
     title, unit = title.split(' - ')
 
-    # remove element which is not a date
-    pattern = 'balance\ssheet+s?'
-    if re.search(pattern, title, flags = re.IGNORECASE) is None:
-        date.pop(0)
+    for i, date in enumerate(facts_hdr):
+        try:
+            parse(date)
+        except:
+            facts_hdr.pop(i)
 
     element_tbl = tbl.find_all(class_ = 'pl')
     element = [x.get_text() for x in element_tbl]
 
     facts_tbl = tbl.find_all(class_ = ['nump', 'num', 'text'])
     facts = [x.get_text().strip() for x in facts_tbl]
-    years = len(date) % len(facts)
+    years = len(facts_hdr) % len(facts)
 
-    facts_consecutive = dict({date[x] : facts[x::years] for x in range(0, years)})
+    consecutive_facts = dict({facts_hdr[x] : facts[x::years] for x in range(0, years)})
 
-    num_of_facts_y = len(facts) // len(date)
+    num_of_facts_y = len(facts) // len(facts_hdr)
     
     if len(element) == num_of_facts_y:
-        facts_df = pd.DataFrame(facts_consecutive, index = element)
+        facts_df = pd.DataFrame(consecutive_facts, index = element)
     else :
         raise Exception('Number of elements and facts doesn\'t match')
 
