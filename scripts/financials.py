c4e83362e00e3fc9bf99fcf20c92fe5c082710d7
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-07-28 15:56:08 +0900

    feat : Add getting unit measure in USD for each financial statement

--- scripts/financials.py
+++ scripts/financials.py
@@ -8,32 +8,30 @@ def get_facts(link):
     soup = BeautifulSoup(res.text, 'html.parser')
     tbl = soup.find('table')
 
-    # date
-    date = tbl.find_all(class_='th')
+    date = tbl.find_all(class_ = 'th')
     date = [x.get_text() for x in date]
 
-    title = tbl.find(class_='tl').get_text()
+    title = tbl.find(class_ = 'tl').get_text()
+    title, unit = title.split(' - ')
 
-    # remove unnecessary
-    if re.search('balance\ssheet+s?', title, flags=re.IGNORECASE) is None:
+    # remove element which is not a date
+    pattern = 'balance\ssheet+s?'
+    if re.search(pattern, title, flags = re.IGNORECASE) is None:
         date.pop(0)
 
-    # element names
-    element_tbl = tbl.find_all(class_='pl')
+    element_tbl = tbl.find_all(class_ = 'pl')
     element = [x.get_text() for x in element_tbl]
-    # facts
-    facts_tbl = tbl.find_all(class_=['nump', 'num', 'text'])
+
+    facts_tbl = tbl.find_all(class_ = ['nump', 'num', 'text'])
     facts = [x.get_text().strip() for x in facts_tbl]
+    years = len(date) % len(facts)
+
+    facts_consecutive = dict({date[x] : facts[x::years] for x in range(0, years)})
 
     num_of_facts_y = len(facts) // len(date)
-    years = len(date)%len(facts)
-
-    # key = date : value = facts
-    facts_consecutive = dict({date[x] : facts[x::years] for x in range(0,years)})
-
-    # check if number of elements in a financial statement match with number of facts
+    
     if len(element) == num_of_facts_y:
-        facts_df = pd.DataFrame(facts_consecutive, index=element)
+        facts_df = pd.DataFrame(facts_consecutive, index = element)
     else :
         raise Exception('Number of elements and facts doesn\'t match')
 
