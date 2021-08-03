d15faaad57da9e79e0b6d8f3846a7e126e761f8a
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-08-03 17:57:35 +0900

    refactor : Modify cik list to store nasdaq and nyse only, and minor changes with formatting company name.

--- scripts/cik.py
+++ scripts/cik.py
@@ -1,13 +1,29 @@
 import pandas as pd
 import request
+import string
+import re
 
 def get_cik():
     url = 'https://www.sec.gov/files/company_tickers_exchange.json'
     data = request.request_data(url, 'json')
     
     cik_list = pd.DataFrame(data['data'], columns=data['fields'])
+    
+    cik_list['exchange'] = cik_list['exchange'].str.upper()
+    cik_list = cik_list[
+        (cik_list['exchange'] == 'NASDAQ') | (cik_list['exchange'] == 'NYSE')
+    ]
+    cik_list = cik_list.reset_index(drop=True)
+    # remove exchange column
+    cik_list = cik_list.iloc[:,:-1]
+
     cik_list['cik'] = cik_list['cik'].astype(str)
-    cik_list['name'] = cik_list['name'].str.lower().str.title()
+
+    # remove all characters after '\' or '/' in a company name
+    pattern = r'\s?(\/|\\)[a-zA-Z]*'
+    regex = re.compile(pattern, flags = re.I)
+    cik_list['name'] = [string.capwords(x) for x in cik_list['name']]
+    cik_list['name'] = [regex.sub('', x) for x in cik_list['name']]
 
     return cik_list
 
@@ -20,4 +36,5 @@ def search_cik(cik_list, ticker):
     # Since cik is a 10-digit number, concatenate 0s.
     zeros = 10 - len(str(data))
     data = ('0' * zeros) + str(data)
+
     return data
