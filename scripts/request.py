b9ad955ef245dc8c7419639ae2003a0e402a379b
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-08-05 16:02:00 +0900

    style : Change checking conditional style

--- scripts/request.py
+++ scripts/request.py
@@ -8,12 +8,12 @@ def request_data(url, ret):
     headers = {'User-Agent': 'Mozilla'}
     res = requests.get(url, headers=headers)
 
-    if res.ok:
-        if ret == 'json':
-            json_file = json.loads(res.text)
-            return json_file
-        elif ret == 'html':
-            soup = BeautifulSoup(res.text, 'html.parser')
-            return soup
-    else:
+    if not res.ok:
         raise Exception('Bad response')
+
+    if ret == 'json':
+        json_file = json.loads(res.text)
+        return json_file
+    elif ret == 'html':
+        soup = BeautifulSoup(res.text, 'html.parser')
+        return soup
