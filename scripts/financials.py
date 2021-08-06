883e86bb40809794065d2b3cbc762e7ee7d07262
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-08-06 17:50:18 +0900

    feat : Add getting data from alpha vantage api

--- scripts/financials.py
+++ scripts/financials.py
@@ -1,7 +1,9 @@
 import yfinance as yf
+import yaml
+import request
 
 
-def get_major_financials(ticker):
+def get_major_financials_yfinance(ticker):
     company = yf.Ticker(ticker)
 
     company.info
@@ -13,3 +15,15 @@ def get_major_financials(ticker):
     company.quarterly_financials
     company.quarterly_balance_sheet
     company.quarterly_cashflow
+
+
+def get_major_financials_alphavantage(ticker):
+    with open('config/config.yml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    api_key = config['user']['alphavantage_api']
+    which = ['OVERVIEW', 'INCOME_STATEMENT', 'BALANCE_SHEET', 'CASH_FLOW']
+    url = ('https://www.alphavantage.co/query?'
+           f'function={which[3]}&symbol={ticker}&apikey={api_key}')
+
+    financial_data = request.request_data(url, 'json')
