a80389e5476cc4968861cd7c65a73bdd20adc8f2
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-08-09 13:49:45 +0900

    refactor : Change source for getting financial data from alpha vantage and yfinance to financialmodelingprep.

--- scripts/financials.py
+++ scripts/financials.py
@@ -1,29 +1,18 @@
-import yfinance as yf
 import yaml
 import request
 
 
-def get_major_financials_yfinance(ticker):
-    company = yf.Ticker(ticker)
-
-    company.info
-
-    company.financials
-    company.balance_sheet
-    company.cashflow
-
-    company.quarterly_financials
-    company.quarterly_balance_sheet
-    company.quarterly_cashflow
-
-
-def get_major_financials_alphavantage(ticker):
+def get_standardized_financials(ticker):
     with open('config/config.yml', 'r') as f:
         config = yaml.safe_load(f)
 
-    api_key = config['user']['alphavantage_api']
-    which = ['OVERVIEW', 'INCOME_STATEMENT', 'BALANCE_SHEET', 'CASH_FLOW']
-    url = ('https://www.alphavantage.co/query?'
-           f'function={which[3]}&symbol={ticker}&apikey={api_key}')
+    api_key = config['user']['financialmodelingprep_api_key']
+    # list 3 major financials for the meantime. Must be modified soon
+    ic = 'income-statement'
+    bs = 'balance-sheet-statement'
+    cf = 'cash-flow-statement'
 
+    base_url = 'https://financialmodelingprep.com/api/v3/'
+    param = f'{ic}/{ticker}?limit=120&apikey={api_key}'
+    url = base_url + param
     financial_data = request.request_data(url, 'json')
