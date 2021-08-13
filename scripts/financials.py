413d95bf5f36274b69dc49f587d68cd8efe04afa
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-08-13 13:35:22 +0900

    refactor : change the source of getting standard financials from nasdaq.com to finviz

--- scripts/financials.py
+++ scripts/financials.py
@@ -2,35 +2,43 @@ import pandas as pd
 from scripts import request
 
 
-def get_std_financials(ticker: str, which_financial: str) -> pd.DataFrame:
-    period = {
-        'annual': '1',
-        'quarter': '2'
-    }
+def get_std_financials(ticker: str,
+                       which_financial: str,
+                       period: str = 'annual') -> dict:
     financials = {
-        'ic': 'incomeStatementTable',
-        'bs': 'balanceSheetTable',
-        'cf': 'cashFlowTable'
+        'income_statement': 'I',
+        'balance_sheet': 'B',
+        'cash_flow': 'C',
     }
-
-    url = ('https://api.nasdaq.com/api/company/'
-           f'{ticker}/financials?frequency={period["annual"]}')
+    periods = {
+        'annual': 'A',
+        'quarter': 'Q',
+    }
+    statement = financials[which_financial] + periods[period]
+    url = f'https://finviz.com/api/statement.ashx?t={ticker}&s={statement}'
     data = request.request_data(url, 'json')
 
-    financial_statement = get_statements(data, financials[which_financial])
+    del data['currency']
+    del data['data']['Period Length']
+
+    financial_statement = get_statements(data)
 
     return financial_statement
 
 
-def get_statements(data: dict, financials_name: str) -> pd.DataFrame:
+def get_statements(data: dict) -> pd.DataFrame():
     try:
-        financial = data['data'][financials_name]
-        df = pd.DataFrame(financial['rows'])
-        df.columns = financial['headers'].values()
-        df.index = df.iloc[:, 0]
-        df.drop(df.columns[[0]], axis=1, inplace=True)
+        df = pd.DataFrame(data['data']).T
+        df.columns = df.iloc[0, :].values
+        df.drop(df.index[[0]], axis=0, inplace=True)
+        df = df.replace(',', '', regex=True)
 
-        return df
+        for i in df:
+            df[i] = pd.to_numeric(df[i])
+
+        values_unit = 1_000_000
+        df = df * values_unit
     except Exception as e:
         print(e)
-        return
+
+    return df
