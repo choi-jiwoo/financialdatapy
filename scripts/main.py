c1a701a7c7d45e795d0fd259ee3ca087a3e6c36b
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-08-13 13:34:37 +0900

    feat : Add getting standard financials of a company

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 from scripts import cik
 from scripts import connect
 from scripts import filings
+from scripts import financials
 
 
 def main():
@@ -11,23 +12,27 @@ def main():
     db.save_in_db(cik_list)
 
     # Getting data from SEC
-    ticker = 'aapl'  # test for apple inc. for meantime
+    ticker = 'aapl'  # test for apple inc for the meantime
     cik_num = cik.search_cik(cik_list, ticker)
 
     # Getting list of submitted filings
     submission = filings.get_filings_list(cik_num)
 
-    form_type = '10-K'
+    form_type = '10-K'  # test 10-K filing for the meantime
+
+    # financial statemets as reported in SEC EDGAR
     income_statement, balance_sheet, cash_flow = filings.get_facts_by_form(
         cik_num,
         submission,
-        form_type  # test for 10-K filing for meantime
+        form_type,
+    )
+    std_financial = financials.get_std_financials(
+        ticker,
+        which_financial='income_statement',  # test income statement for the meantime
+        period='annual',
     )
 
     # Do something with these financial statements data...
-    print(income_statement)
-    print(balance_sheet)
-    print(cash_flow)
 
 
 if __name__ == '__main__':
