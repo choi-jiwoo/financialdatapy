48550933d647a08c5130df6782a5dfe5ab95acf2
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-08-20 16:34:31 +0900

    refactor : Parameterize which RDB and DBAPI to use for the database connection

--- scripts/connect.py
+++ scripts/connect.py
@@ -6,14 +6,18 @@ def connect_db() -> Database:
     with open('config/config.yml', 'r') as f:
         config = yaml.safe_load(f)
 
+    rdb = 'mysql'
+    dbapi = 'pymysql'
     user = config['user']['user']
     mysql_pw = config['user']['mysql_password']
     host = config['user']['host']
-    db_name = 'us_stock'
-    table_name = 'stock_list'
+    db = 'us_stock'
+    table = 'stock_list'
 
     try:
-        db = Database(user, mysql_pw, host, db_name, table_name)
+        db = Database(rdb, dbapi,
+                      user, mysql_pw, host,
+                      db, table)
     except Exception as e:
         print(e)
     else:
