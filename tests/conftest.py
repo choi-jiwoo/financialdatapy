cd252d731346b782ef76b603b7575fd1e4ec0769
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-08-30 14:54:39 +0900

    docs(fixture): add more descriptions in fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,17 @@ from financialdatapy import stock
 
 @pytest.fixture
 def cik_list():
-    """Get CIK list and use it as a fixture."""
+    """Get CIK list and use it as a fixture.
+
+    Used in TestCik inside test_integration.py.
+    """
     return cik.get_cik()
 
 
 @pytest.fixture
-def company(self):
-    """Instantiate Stock class."""
+def company():
+    """Instantiate Stock class.
+
+    Used in TestFinancials inside test_integration.py.
+    """
     return stock.Stock('AAPL')
