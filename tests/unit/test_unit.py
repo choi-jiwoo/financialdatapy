1e46719ef61c10661c6a1b55792fc908c27eeb5f
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-08-30 11:29:11 +0900

    test(date): test parsed date returns corresponding timestamp value

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -4,5 +4,5 @@ from financialdatapy import price
 
 def test_parsing_date():
     date = price.Price('AAPL', (2021, 8, 1), (2021, 8, 10))
-    res = date.parse_date()
-    assert all(isinstance(x, datetime.date) for x in res.values())
+    assert date.start == 1627743600
+    assert date.end == 1628521200
