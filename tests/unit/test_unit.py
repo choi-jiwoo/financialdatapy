867bb848062fd4659a97f75e3dd47a35ce0f8f8d
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-08-31 11:58:51 +0900

    test(date): change the format of start and end date to pass as arguments to str from tuple

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -3,6 +3,7 @@ from financialdatapy import price
 
 
 def test_parsing_date():
-    date = price.Price('AAPL', (2021, 8, 1), (2021, 8, 10))
-    assert date.start == 1627743600
+    """Test the date correctly converts to timestamp."""
+    date = price.Price('AAPL', '2021-8-3', '2021-8-10')
+    assert date.start == 1627963200
     assert date.end == 1628521200
