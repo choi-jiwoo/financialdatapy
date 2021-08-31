4c5bd55e66f007f05fdf7e608317d2b581ef80e1
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-08-31 12:33:16 +0900

    test: set correct timestamp to make assert statement be true

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -6,4 +6,4 @@ def test_parsing_date():
     """Test the date correctly converts to timestamp."""
     date = price.Price('AAPL', '2021-8-3', '2021-8-10')
     assert date.start == 1627963200
-    assert date.end == 1628521200
+    assert date.end == 1628654400
