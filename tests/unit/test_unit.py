851f03b59e3a331295927eaeba42a3bf552e2311
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-08-31 15:03:09 +0900

    test(date): parametrize start and end date inputs to be in different formats

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -1,9 +1,18 @@
-import datetime
+import pytest
 from financialdatapy import price
 
 
-def test_parsing_date():
+@pytest.mark.parametrize(
+    'start, end',
+    [
+        ('2021-8-3', '2021-8-10'),
+        ('2021-08-03', '2021-08-10'),
+        ('21-8-3', '21-8-10'),
+        ('21-08-03', '21-08-10'),
+    ]
+)
+def test_parsing_date(start, end):
     """Test the date correctly converts to timestamp."""
-    date = price.Price('AAPL', '2021-8-3', '2021-8-10')
+    date = price.Price('AAPL', start, end)
     assert date.start == 1627963200
     assert date.end == 1628654400
