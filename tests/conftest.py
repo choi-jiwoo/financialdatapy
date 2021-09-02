8f54939bc3131626857b66dc1121edadf05fb9c8
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-09-02 13:52:30 +0900

    test(fixture): move fixture for getting cik list into test_integration.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,15 +4,6 @@ from financialdatapy import stock
 
 
 @pytest.fixture
-def cik_list():
-    """Get CIK list and use it as a fixture.
-
-    Used in TestCik inside test_integration.py.
-    """
-    return cik.get_cik()
-
-
-@pytest.fixture
 def company():
     """Instantiate Stock class.
 
