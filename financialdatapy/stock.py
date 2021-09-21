48aeaef1d15156f9306055ece0f4807d01c0b1db
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-09-21 22:05:12 +0900

    docs: change docstring style to sphinx

--- financialdatapy/stock.py
+++ financialdatapy/stock.py
@@ -1,35 +1,4 @@
-"""Retrieve data of a certain stock.
-
-Scrape data from 3 major financial statements as reported in the SEC EDGAR
-system, 3 major standard financial statements from finviz.com,
-and a historical price data of a stock (company).
-
-Classes:
-    Cik
-    Stock
-
-Get a list of CIK:
-
-    from financialdatapy.stock import Cik
-
-    Cik.update_cik_list()  # get latest cik list
-    cik_list = Cik.cik_list
-
-Get financial statements:
-
-    from financialdatapy.stock import Stock
-
-    comp = Stock('AAPL')
-    aapl_ic_as_reported = comp.financials('10-K', 'income_statement')
-    aapl_std_ic = comp.standard_financials('income_statement', 'annual')
-
-Get historical price data:
-
-    from financialdatapy.stock import Stock
-
-    comp = Stock('AAPL')
-    price = comp.historical('2021-1-1', '2021-2-1')
-"""
+"""This module retrieves financial data of a stock."""
 import pandas as pd
 from typing import Optional
 from financialdatapy.cik import get_cik_list
@@ -40,29 +9,43 @@ from financialdatapy.price import UsMarket
 
 
 class Cik():
-    """Cik list as a class variable."""
+    """Cik list as a class variable.
 
+    :Example:
+
+    >>> from financialdatapy.stock import Cik
+    >>> cik_list = Cik.cik_list
+    >>> cik_list
+       cik|       name| ticker
+    ------|-----------|-------
+    320193| Apple Inc.|   AAPL
+    ...
+    """
+
+    #: Get the list of cik of companies registered in SEC.
     cik_list = get_cik_list()
 
     @staticmethod
     def update_cik_list():
-        """Get the latest cik list from SEC."""
+        """Update cik list from SEC to the latest.
+
+        :Example:
+
+        >>> from financialdatapy.stock import Cik
+        >>> Cik.update_cik_list()
+        """
         Cik.cik_list = get_cik_list(update=True)
 
 
 class Stock(Cik):
-    """Class representing a stock or a company.
+    """A class representing a stock or a company.
 
-    Attributes:
-        ticker: Ticker of a company/stock.
+    :param ticker: Ticker of a company/stock.
+    :type ticker: str
     """
 
     def __init__(self, ticker: str) -> None:
-        """Initialize ticker to search.
-
-        Args:
-            ticker: Ticker of a stock.
-        """
+        """Initialize ticker to search."""
 
         self.ticker = ticker.upper()
 
@@ -70,14 +53,27 @@ class Stock(Cik):
                    financial: str = 'income_statement') -> pd.DataFrame:
         """Get financial statements as reported.
 
-        Args:
-            form: Either '10-K' or '10-Q' form. Default value is '10-K'.
-            financial: Which financial statement to retrieve. Input string
+        :param form: Either '10-K' or '10-Q' form. Default value is '10-K'.
+        :type form: str, optional
+        :param financial: Which financial statement to retrieve. Input string
                 should be either 'income_statement', 'balance_sheet', or
                 'cash_flow'. Default value is 'income_statement'
+        :type financial: str, optional
 
-        Returns:
-            A dataframe containing financial statement as reported.
+        :return: A dataframe containing financial statement as reported.
+        :rtype: pandas.DataFrame
+
+        :Example:
+
+        >>> from financialdatapy.stock import Stock
+        >>> comp = Stock('AAPL')
+        >>> ic_as_reported = comp.financials('10-K', 'income_statement')
+        >>> ic_as_reported
+             CONSOLIDATED STATEMENTS OF OPERATIONS| 12 Months Ended
+        USD ($) shares in Thousands, $ in Millions|   Sep. 26, 2020| Sep. 28, 2019| Sep. 29, 2018
+        ------------------------------------------|----------------|--------------|--------------
+                                         Net sales|          274515|        260174|        265595
+        ...
         """
 
         comp_cik = search_cik(Stock.cik_list, self.ticker)
@@ -97,15 +93,27 @@ class Stock(Cik):
                             period: str = 'annual') -> pd.DataFrame:
         """Get standard financial statements.
 
-        Args:
-            which_financial: One of the three financial statement.
-                'income_statement' or 'balance_sheet' or 'cash_flow'. Default
-                value is 'income_statement'
-            period: Either 'annual' or 'quarter'. Default value is 'annual'
+        :param which_financial: One of the three financial statement.
+            'income_statement' or 'balance_sheet' or 'cash_flow'. The default
+            value is 'income_statement'
+        :type which_financial: str, optional
+        :param period: Either 'annual' or 'quarter'. The default value is 'annual'
+        :type period: str, optional
 
-        Returns:
-            Dataframe with columns as dates, and index as financial
+        :return: A dataframe with columns as dates, and index as financial
             statement elements.
+        :rtype: pandas.DataFrame
+
+        :Example:
+
+        >>> from financialdatapy.stock import Stock
+        >>> comp = Stock('AAPL')
+        >>> std_ic = comp.standard_financials('income_statement', 'annual')
+        >>> std_ic
+                     |          TTM|    9/26/2020| ...
+        -------------|-------------|-------------|----
+        Total Revenue| 3.471550e+11| 2.745150e+11| ...
+        ...
         """
 
         market = UsFinancials()
@@ -121,16 +129,26 @@ class Stock(Cik):
                    end: Optional[str] = None) -> pd.DataFrame:
         """Get historical stock price data.
 
-        Args:
-            start: Start date to query. Format should be in ISO 8601.
-                e.g. 2021-8-1 or 2021-08-01
-                If empty, 1900-01-01 is passed.
-            end: End date to query. Format should be in ISO 8601.
-                e.g. 2021-8-10 or 2021-08-10
-                If empty, date of today is passed.
+        :param start: Start date to query. Format should be in ISO 8601.
+            If empty, 1900-01-01 is passed.
+        :type start: str, optional
+        :param end: End date to query. Format should be in ISO 8601.
+            If empty, date of today is passed.
+        :type end: str, optional
 
-        Returns:
-            Historical stock price data in dataframe.
+        :return: Historical stock price data in dataframe.
+        :rtype: pandas.DataFrame
+
+        :Example:
+
+        >>> from financialdatapy.stock import Stock
+        >>> comp = Stock('AAPL')
+        >>> price = comp.historical('2021-1-1', '2021-1-5')
+        >>> price
+                  |  close|   open|   high|    low|    volume
+        ----------|-------|-------|-------|-------|----------
+        2021-01-04| 129.41| 133.52| 133.61| 126.76| 143301900
+        ...
         """
         price = UsMarket(self.ticker, start, end)
         price_data = price.get_price_data()
