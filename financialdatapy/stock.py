ed11da1bfadc99b3890aaacbe9c4d89d0ef7d68e
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-10-10 16:37:50 +0900

    refactor: Move class StockList to stocklist.py

--- financialdatapy/stock.py
+++ financialdatapy/stock.py
@@ -3,11 +3,6 @@ import pandas as pd
 import re
 from typing import Optional
 from financialdatapy.country import Market
-from financialdatapy.cik import get_stock_list
-from financialdatapy.cik import UsStockList
-from financialdatapy.filings import get_filings_list
-from financialdatapy.financials import UsFinancials
-from financialdatapy.price import UsMarket
 
 
 class CountryCodeValidationFailed(Exception):
@@ -15,55 +10,7 @@ class CountryCodeValidationFailed(Exception):
     pass
 
 
-class StockList():
-    """Stock list as a class variable.
-
-    :Example:
-
-    >>> from financialdatapy.stock import StockList
-    >>> us_stock_list = StockList.us_stock_list
-    >>> us_stock_list
-       cik|       name| ticker
-    ------|-----------|-------
-    320193| Apple Inc.|   AAPL
-    """
-
-    us_stock_list = get_stock_list(UsStockList())
-
-    @staticmethod
-    def update_stock_list():
-        """Update stock list to the latest.
-
-        :Example:
-
-        >>> from financialdatapy.stock import StockList
-        >>> StockList.update_stock_list()
-        """
-        StockList.us_stock_list = get_stock_list(UsStockList(), update=True)
-
-    @staticmethod
-    def search_cik(symbol: str) -> str:
-        """Search CIK of specific a company.
-
-        :param symbol: Company symbol to search.
-        :type symbol: str
-        :return: CIK of the company searching for.
-        :rtype: str
-        """
-        symbol = symbol.upper()
-        stock_list = StockList.us_stock_list
-        symbol_df = stock_list[stock_list['ticker'] == symbol]
-        cik = symbol_df['cik'].item()
-
-        # cik number received from source excludes 0s that comes first.
-        # Since cik is a 10-digit number, concatenate 0s.
-        zeros = 10 - len(str(cik))
-        cik = ('0' * zeros) + str(cik)
-
-        return cik
-
-
-class Stock(StockList):
+class Stock():
     """A class representing a stock or a company.
 
     :param symbol: Symbol of a company/stock.
