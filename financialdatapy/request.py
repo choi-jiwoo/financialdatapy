a59db1b8b20338a23bc513258e6e28cf3bf490e5
choi jiwoo <47313851+cho2ji@users.noreply.github.com>
2021-11-07 16:19:36 +0900

    feat: add POST method

--- financialdatapy/request.py
+++ financialdatapy/request.py
@@ -1,4 +1,5 @@
 """This module requests data from web."""
+from typing import Optional
 import requests
 import json
 from bs4 import BeautifulSoup
@@ -9,22 +10,37 @@ class Request():
 
     :param url: Url of the data source.
     :type url: str
+    :param method: Which http methods to request, defaults to 'get'.
+    :type method: str, optional
+    :param form_data: Form data to pass when making POST request, defaults to None
+    :type form_data: Optional[dict], optional
     """
 
-    def __init__(self, url: str) -> None:
+    def __init__(self, url: str, method: str = 'get',
+                 form_data: Optional[dict] = None) -> None:
         """Initialize Response object."""
         self.url = url
-        self.res = self.http_request()
+        self.form_data = form_data
+        self.res = self.http_request(method)
 
-    def http_request(self) -> requests.Response:
+    def http_request(self, method: str) -> requests.Response:
         """Sends a GET request to a data source url.
 
+        :param method: Which http methods to request.
+        :type method: str
         :raises ConnectionError: If the connection failed.
         :return: A response object from the source.
         :rtype: requests.Response
         """
-        headers = {'User-Agent': 'Mozilla'}
-        res = requests.get(self.url, headers=headers)
+        headers = {
+            'User-Agent': 'Mozilla',
+            'X-Requested-With': 'XMLHttpRequest',
+        }
+
+        if method == 'post':
+            res = requests.post(self.url, data=self.form_data, headers=headers)
+        else:
+            res = requests.get(self.url, headers=headers)
 
         if res.status_code != 200:
             res.raise_for_status()
