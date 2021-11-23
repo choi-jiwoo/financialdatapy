701ca999155b00c1857738772f8c84e3c9cd3e83
choi jiwoo <cho2.jiwoo@gmail.com>
2021-11-23 23:14:15 +0900

    build: improve setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,47 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages
+import os
+import sys
 
-setup(name="financialdatapy", packages=find_packages())
+
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+
+description = 'Financial data of a company.'
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+install_requires = [
+    '',
+]
+
+project_urls = {
+  'Source': 'https://github.com/choi-jiwoo/financialdatapy',
+}
+
+setup(
+    name='financialdatapy',
+    version='0.1.0',
+    author='Choi Jiwoo',
+    author_email='cho2.jiwoo@gmail.com',
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    keywords=['python', 'stock', 'finance'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    project_urls=project_urls,
+)
