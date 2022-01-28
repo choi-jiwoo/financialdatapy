99439ffb3a0d468563338811f21a3257e57a562f
choi jiwoo <cho2.jiwoo@gmail.com>
2022-01-28 12:07:14 +0900

    refactor: delete conftest.py

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from financialdatapy import cik
-from financialdatapy import stock
-
-
-@pytest.fixture(scope='module')
-def company():
-    """Instantiate Stock class."""
-    return stock.Stock('AAPL')
