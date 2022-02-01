73aaced078292e984c17c0ec8a58495095c5e501
choi jiwoo <cho2.jiwoo@gmail.com>
2022-02-01 11:34:19 +0900

    feat: add getting company code of a stock in Korea Exchange

--- financialdatapy/stocklist.py
+++ financialdatapy/stocklist.py
@@ -1,5 +1,11 @@
 """This module retrieves stock lists."""
+from io import BytesIO
+import json
+from functools import lru_cache
 import pandas as pd
+import xmltodict
+from zipfile import ZipFile
+from financialdatapy.request import Request
 from financialdatapy.cik import UsCikList
 
 
@@ -47,3 +53,26 @@ class StockList:
         cik = ('0' * zeros) + str(cik)
 
         return cik
+
+    @staticmethod
+    @lru_cache
+    def get_comp_code_list(api_key: str) -> str:
+        url = 'https://opendart.fss.or.kr/api/corpCode.xml'
+        params = {
+            'crtfc_key': api_key
+        }
+        res = Request(url, params=params)
+        zip_file = res.get_content()
+        try:
+            xml_zip_file = ZipFile(BytesIO(zip_file))
+            xml_file = xml_zip_file.read('CORPCODE.xml').decode('utf-8')
+            raw_corp_code = xmltodict.parse(xml_file)
+            encoded_corp_code = json.dumps(raw_corp_code)
+            corp_code = json.loads(encoded_corp_code)
+
+            corp_code_list = pd.DataFrame(corp_code['result']['list'])
+            corp_code_list.dropna(inplace=True)
+
+            return corp_code_list
+        except Exception as e:
+            print(e)
