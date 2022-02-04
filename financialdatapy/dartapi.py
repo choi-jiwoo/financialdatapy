a0c69dacfacd5d9958d17d95e111d48e3029bf91
choi jiwoo <cho2.jiwoo@gmail.com>
2022-02-04 15:07:28 +0900

    feat: add raising custom EmptyApiKeyException when .env is missing

--- financialdatapy/dartapi.py
+++ financialdatapy/dartapi.py
@@ -3,6 +3,15 @@ from typing import Optional
 import os
 
 
+class EmptyApiKeyException(Exception):
+
+    def __init__(self, msg: Optional[str] = None, *args, **kwargs):
+        if msg is None:
+            msg = ('No API key is provided.'
+                   'Check out the documentation for more details.')
+        super().__init__(msg, *args, **kwargs)
+
+
 class Dart:
 
     def __init__(self, dart_api_key: Optional[str] = None):
@@ -15,10 +24,9 @@ class Dart:
     @dart_api_key.setter
     def dart_api_key(self, dart_api_key: Optional[str] = None):
         if dart_api_key is None:
-            try:
-                load_dotenv()
-                self._dart_api_key = os.environ.get('DART_API_KEY')
-            except Exception as e:
-                raise RuntimeError('Empty API key.') from e
+            load_dotenv()
+            self._dart_api_key = os.environ.get('DART_API_KEY')
+            if self._dart_api_key is None:
+                raise EmptyApiKeyException()
         else:
             self._dart_api_key = dart_api_key
