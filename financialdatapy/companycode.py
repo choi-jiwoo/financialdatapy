b6c8ccd9a36842498d0f012988370440aed6cbf2
choi jiwoo <cho2.jiwoo@gmail.com>
2022-02-06 01:13:34 +0900

    refactor: add CompanyCode class and put search_code as its method

--- financialdatapy/companycode.py
+++ financialdatapy/companycode.py
@@ -1,20 +1,31 @@
+"""This module retrieves company code with its company name."""
 from financialdatapy.request import Request
 
 
-def search_code(name: str) -> str:
-    url = 'https://kind.krx.co.kr/common/searchcorpname.do'
-    data = {
-        'method': 'searchCorpNameJson',
-        'searchCodeType': 'char',
-        'searchCorpName': name,
-    }
-    headers = {
-        'User-Agent': 'Mozilla',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    res = Request(url, method='post', headers=headers, data=data)
-    comp_info_first_result = res.get_json()[0]
-    company_code = comp_info_first_result['repisusrtcd2']
+class CompanyCode:
 
-    return company_code
+    @staticmethod
+    def search_code(name: str) -> str:
+        """Search company code with company name in dart.fss.or.kr.
+
+        :param name: Company name.
+        :type name: str
+        :return: Company code.
+        :rtype: str
+        """
+        url = 'https://kind.krx.co.kr/common/searchcorpname.do'
+        data = {
+            'method': 'searchCorpNameJson',
+            'searchCodeType': 'char',
+            'searchCorpName': name,
+        }
+        headers = {
+            'User-Agent': 'Mozilla',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        res = Request(url, method='post', headers=headers, data=data)
+        comp_info_first_result = res.get_json()[0]
+        company_code = comp_info_first_result['repisusrtcd2']
+
+        return company_code
