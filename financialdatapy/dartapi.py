9ca8e45f94c922f04b7346e651d5d2223b86972c
choi jiwoo <cho2.jiwoo@gmail.com>
2022-02-06 15:26:53 +0900

    style: add function annotation

--- financialdatapy/dartapi.py
+++ financialdatapy/dartapi.py
@@ -5,7 +5,7 @@ import os
 
 class EmptyApiKeyException(Exception):
 
-    def __init__(self, msg: Optional[str] = None, *args, **kwargs):
+    def __init__(self, msg: Optional[str] = None, *args, **kwargs) -> None:
         if msg is None:
             msg = ('No API key is provided.'
                    'Check out the documentation for more details.')
@@ -14,15 +14,15 @@ class EmptyApiKeyException(Exception):
 
 class Dart:
 
-    def __init__(self, dart_api_key: Optional[str] = None):
+    def __init__(self, dart_api_key: Optional[str] = None) -> None:
         self.dart_api_key = dart_api_key
 
     @property
-    def dart_api_key(self):
+    def dart_api_key(self) -> str:
         return self._dart_api_key
 
     @dart_api_key.setter
-    def dart_api_key(self, dart_api_key: Optional[str] = None):
+    def dart_api_key(self, dart_api_key: Optional[str] = None) -> None:
         if dart_api_key is None:
             load_dotenv()
             self._dart_api_key = os.environ.get('DART_API_KEY')
