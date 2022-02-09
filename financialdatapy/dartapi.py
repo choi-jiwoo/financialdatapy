624f6d62473885fc11260afb5bff9679de90daa0
choi jiwoo <cho2.jiwoo@gmail.com>
2022-02-09 23:44:51 +0900

    style: rename dart_api_key to api_key

--- financialdatapy/dartapi.py
+++ financialdatapy/dartapi.py
@@ -8,35 +8,35 @@ from financialdatapy.exception import EmptyApiKeyException
 class Dart:
     """This class represents api key from opendart.fss.or.kr.
 
-    :param dart_api_key: Api key from opendart.fss.or.kr, defaults to None.
-    :type dart_api_key: Optional[str], optional
+    :param api_key: Api key from opendart.fss.or.kr, defaults to None.
+    :type api_key: Optional[str], optional
     """
 
-    def __init__(self, dart_api_key: Optional[str] = None) -> None:
+    def __init__(self, api_key: Optional[str] = None) -> None:
         """Initialize Dart."""
-        self.dart_api_key = dart_api_key
+        self.api_key = api_key
 
     @property
-    def dart_api_key(self) -> str:
-        """Getter method of property dart_api_key.
+    def api_key(self) -> str:
+        """Getter method of property api_key.
 
         :return: Api key from opendart.fss.or.kr
         :rtype: str
         """
-        return self._dart_api_key
+        return self._api_key
 
-    @dart_api_key.setter
-    def dart_api_key(self, dart_api_key: Optional[str] = None) -> None:
-        """Setter method of property dart_api_key.
+    @api_key.setter
+    def api_key(self, api_key: Optional[str] = None) -> None:
+        """Setter method of property api_key.
 
-        :param dart_api_key: Api key from opendart.fss.or.kr, defaults to None.
-        :type dart_api_key: Optional[str], optional
+        :param api_key: Api key from opendart.fss.or.kr, defaults to None.
+        :type api_key: Optional[str], optional
         :raises EmptyApiKeyException: Api key is not submitted.
         """
-        if dart_api_key is None:
+        if api_key is None:
             load_dotenv()
-            self._dart_api_key = os.environ.get('DART_API_KEY')
-            if self._dart_api_key is None:
+            self._api_key = os.environ.get('DART_API_KEY')
+            if self._api_key is None:
                 raise EmptyApiKeyException()
         else:
-            self._dart_api_key = dart_api_key
+            self._api_key = api_key
